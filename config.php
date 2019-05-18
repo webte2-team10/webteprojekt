4a4b45ad4c37ef64003575fb35332cf9d1bb6092
xklementova <45430059+xklementova@users.noreply.github.com>
2019-05-18 23:31:54 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,31 +1,7 @@
-<<<<<<< HEAD
 <?php
 
 $servername = "localhost";
 $username = "xklementova";
 $password = "hye1YXkleJ1O";
-$dbname = "webtech_final";          //oni tam maju dbname a zakova tam ma este viac
-$charset = 'utf8';                  // must be utf8, database, main page is all in utf8
-
-/*
-$hostname = 'localhost:3306';
-$database = 'webtech_final';
-$username = 'davzak';
-$password = 'heslo123';
-*/
-=======
-<?php
-
-$servername = "localhost";
-$username = "xklementova";
-$password = "hye1YXkleJ1O";
-$dbname = "webtech_final";          //oni tam maju dbname a zakova tam ma este viac
-$charset = 'utf8';                  // must be utf8, database, main page is all in utf8
-
-/*
-$hostname = 'localhost:3306';
-$database = 'webtech_final';
-$username = 'davzak';
-$password = 'heslo123';
-*/
->>>>>>> 383ae415c9f4d9fe95f8d78d2d81e55ccee6ed9d
+$dbname = "webtech_final";  
+$charset = 'utf8';                 
