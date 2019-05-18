16ef8641101fcfb0eb72f0b41af8be02b7ed6179
xklementova <45430059+xklementova@users.noreply.github.com>
2019-05-18 23:33:44 +0200

    Update student_home.php

--- student_home.php
+++ student_home.php
@@ -1,4 +1,3 @@
-
 <?php
 
 //dat na zaciatok stranok, ktore maju byt pristupne len pre studentov
@@ -14,47 +13,37 @@ if (!isLoggedIn()) {
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <a href="student_home.php">Home</a>
-            <ul>
-                <li><a href="uloha1/uloha1.php">Task 1</a></li>
-            </ul>
-            <ul>
-                <li><a href="student_task2.php">Task 2</a></li>
-            </ul>
-            <ul>
-                <li><a href="misc.php">Choose a title</a></li>
-            </ul>
-            <ul>
-                <li><a href="student_home.php?logout='1'">Log out</a></li>
-            </ul>
-        </nav>
-    </header>
+
+<?php
+$currentPage = "Home";
+include('navbar.php');
+?>
 
     <main>
-        <h2>Home Page</h2>
-        <!-- notification message -->
-        <?php if (isset($_SESSION['success'])) : ?>
+        <div class="container">
+            <h2>Webový portál študenta</h2>
+            <!-- notification message -->
+            <?php if (isset($_SESSION['success'])) : ?>
+                <div>
+                    <h3>
+                        <?php
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                        ?>
+                    </h3>
+                </div>
+            <?php endif ?>
+            <!-- logged in user information -->
             <div>
-                <h3>
-                    <?php
-                    echo $_SESSION['success'];
-                    unset($_SESSION['success']);
-                    ?>
-                </h3>
+                <?php
+                echo "<br>Meno prihláseného používateľa: ".$_SESSION['user']['name'];
+                ?>
             </div>
-        <?php endif ?>
-        <!-- logged in user information -->
-        <div>
-            <?php
-            echo "<br>Your name: ".$_SESSION['user']['name'];
-            echo "<br>Type of user: ".$_SESSION['user']['type_of_user'];
-            ?>
         </div>
     </main>
 </body>
-
 </html>
