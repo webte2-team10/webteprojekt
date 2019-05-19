e07461aa0cb4f70982dd442061665e09e4483d18
xklementova <45430059+xklementova@users.noreply.github.com>
2019-05-19 14:02:51 +0200

    Update misc.php

--- misc.php
+++ misc.php
@@ -2,6 +2,8 @@
 
 //dat na zaciatok stranok, ktore maju byt pristupne len pre studentov
 include('functions.php');
+include('lang.php');    //treba includnut aj script.js
+
 if (!isLoggedIn() && !isAdmin()) {
     $_SESSION['msg'] = "You must log in first ಠ_ಠ";
     header('location: login.php');
@@ -12,9 +14,10 @@ if (!isLoggedIn() && !isAdmin()) {
 <html lang=sk>
 <head>
     <meta charset="UTF-8">
-    <title>Ďalšie info</title>
+    <title><?= _TileMisc ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <script src="scripts/js/script.js"></script>
 </head>
 <body>
 
@@ -25,36 +28,36 @@ include('navbar.php');
 
 <main>
     <div class="container">
-        <h2>Ďalšie info</h2>
-        <a href="https://147.175.121.210:4472/cvicenia/projekt/technicka_dokumentacia">Technická dokumentácia</a>
-        <h3>Rozdelenie úloh medzi študentov</h3>
+        <h2><?= _DalsieInfo ?></h2>
+        <a href="https://147.175.121.210:4472/cvicenia/projekt/technicka_dokumentacia"><?= _TechDok?></a>
+        <h3><?= _RozdelenieUloh ?></h3>
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th>Meno</th>
-                <th>Úloha</th>
+                <th><?= _Meno ?></th>
+                <th><?= _Uloha?></th>
             </tr>
             </thead>
             <tbody>
             <tr>
                 <td>Dávid Zakhariás</td>
-                <td>úloha 1</td>
+                <td><?= _UlohaDavid ?></td>
             </tr>
             <tr>
                 <td>Michal Morávek</td>
-                <td>úloha 2 - pohľad študenta? </td>
+                <td><?= _UlohaMichal ?></td>
             </tr>
             <tr>
                 <td>Tomáš Macho</td>
-                <td>úloha 3</td>
+                <td><?= _UlohaTomas ?></td>
             </tr>
             <tr>
                 <td>Natália Klementová</td>
-                <td>prihlasovanie, spájanie/navigácia?</td>
+                <td><?= _UlohaNaty ?></td>
             </tr>
             <tr>
                 <td>Erik Hricka</td>
-                <td>úloha 2 -  pohľad administrátora? </td>
+                <td><?= _UholaErik ?> </td>
             </tr>
             </tbody>
         </table>
