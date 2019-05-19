c1e5c34a8c72d49805c876bd3527b75395b48222
xklementova <45430059+xklementova@users.noreply.github.com>
2019-05-19 14:03:42 +0200

    Add files via upload

---
+++ lang_en.php
@@ -0,0 +1,38 @@
+<?php
+//navbar.php
+define("_WebPortalAdmin", "Web portal for admin");
+define("_WebPortalStudent", "Web portal for student");
+define("_Task1Admin", "Display the results");
+define("_Task2Admin", "Points assingnemnt");
+define("_Task3Admin", "Task 3");
+define("_Task1Student", "Display the results");
+define("_Task2Student", "Task 2");
+define("_Odhlasit", "Log out");
+
+
+//home
+define("_TitleLogin", "Login");
+define("_Prihlasit", "Log in to the web portal");
+define("_Login", "Login");
+define("_Password", "Password");
+define("_MenoPrihPouz", "Name of the user: ");
+define("_TitleHome", "Home");
+define("_TlacidloPrihlasit", "Log in");
+define("_LoginPlaceholder", "Enter your login");
+define("_PwdPlaceholder", "Enter your password");
+
+
+
+
+//misc.php
+define("_TileMisc", "More info");
+define("_DalsieInfo", "More info");
+define("_TechDok", "Address for documentation");
+define("_RozdelenieUloh", "Division of tasks among students");
+define("_Meno", "Name");
+define("_Uloha", "Type of task");
+define("_UlohaDavid", "task 1");
+define("_UlohaMichal", "task 2 - admin view");
+define("_UlohaTomas", "task 3");
+define("_UlohaNaty", "login, header, bilingualism ");
+define("_UholaErik", "task 2 - student view, bilingualism");
---
+++ lang_sk.php
@@ -0,0 +1,44 @@
+<?php
+//navbar.php
+define("_WebPortalAdmin", "Webový portál admina");
+define("_WebPortalStudent", "Webový portál študenta");
+define("_Task1Admin", "Zobrazenie výsledkov");
+define("_Task2Admin", "Prideľovanie bodov");
+define("_Task3Admin", "Úloha 3");
+define("_Task1Student", "Zobrazenie výsledkov");
+define("_Task2Student", "Úloha 2");
+define("_Odhlasit", "Odhlásiť sa");
+
+
+//home
+define("_TitleLogin", "Prihlásenie");
+define("_Prihlasit", "Prihlásenie do webového portálu");
+define("_Login", "Login");
+define("_Password", "Heslo");
+define("_MenoPrihPouz", "Meno používateľa: ");
+define("_TitleHome", "Domov");
+define("_TlacidloPrihlasit", "Prihlásiť sa");
+define("_LoginPlaceholder", "Zadajte svoj login");
+define("_PwdPlaceholder", "Zadajte svoje heslo");
+
+
+//misc.php
+define("_TileMisc", "Ďalšie info");
+define("_DalsieInfo", "Ďalšie info");
+define("_TechDok", "Linka na technickú dokumentáciu");
+define("_RozdelenieUloh", "Rozdelenie úloh medzi študentov");
+define("_Meno", "Meno");
+define("_Uloha", "Úloha");
+define("_UlohaDavid", "úloha 1");
+define("_UlohaMichal", "úloha 2 - pohľad administrátora ");
+define("_UlohaTomas", "úloha 3");
+define("_UlohaNaty", "prihlasovanie, spájanie/navigácia?, jazyky");
+define("_UholaErik", "úloha 2 -  pohľad študenta, jazyky");
+
+
+
+
+/*
+define("_", "");
+define("_", "");
+*/
